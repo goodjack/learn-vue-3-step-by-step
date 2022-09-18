8f8aaac0a94c22a84e7a4623072743ed1cd3ccca
小克 <goodjack@users.noreply.github.com>
2022-09-18 16:14:27 +0800

    Episode 7: Bring it All Together
    https://gist.github.com/JeffreyWay/7ebe043f72ddd64fb54528c74c102bc7
    https://laracasts.com/series/learn-vue-3-step-by-step/episodes/7

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,9 @@
-import AppButton from "./AppButton.js";
+import Assignments from "./Assignments.js";
 
 export default {
-  components: {
-    "app-button": AppButton,
-  },
+  components: { Assignments },
+
+  template: `
+        <assignments></assignments>
+    `,
 };
