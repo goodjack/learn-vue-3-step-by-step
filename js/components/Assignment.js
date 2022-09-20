fb5005747f01f395377cefb91f21e61bae1254d7
小克 <goodjack@users.noreply.github.com>
2022-09-20 20:58:31 +0800

    Episode 9: Parent-Child State Communication
    https://gist.github.com/JeffreyWay/78f3bbac4564d2506916c1190b488aa7
    https://laracasts.com/series/learn-vue-3-step-by-step/episodes/9

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,13 +1,13 @@
 export default {
   template: `
-        <li>
-            <label class="p-2 flex justify-between items-center">
-                {{ assignment.name }}
+      <li>
+          <label class="p-2 flex justify-between items-center">
+              {{ assignment.name }}
 
-                <input type="checkbox" v-model="assignment.complete" class="ml-3">
-            </label>
-        </li>
-    `,
+              <input type="checkbox" v-model="assignment.complete" class="ml-3">
+          </label>
+      </li>
+  `,
 
   props: {
     assignment: Object,
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-            <form @submit.prevent="add">
-                <div class="border border-gray-600 text-black">
-                    <input v-model="newAssignment" placeholder="New assignment..." class="p-2"/>
-                    <button type="submit" class="bg-white p-2 border-l">Add</button>
-                </div>
-            </form>
+            <assignment-create @add="add"></assignment-create>
         </section>
     `,
 
@@ -24,8 +20,6 @@ export default {
         { name: "Read Chapter 4", complete: false, id: 2 },
         { name: "Turn in Homework", complete: false, id: 3 },
       ],
-
-      newAssignment: "",
     };
   },
 
@@ -41,14 +35,12 @@ export default {
   },
 
   methods: {
-    add() {
+    add(name) {
       this.assignments.push({
-        name: this.newAssignment,
+        name: name,
         completed: false,
         id: this.assignments.length + 1,
       });
-
-      this.newAssignment = "";
     },
   },
 };
